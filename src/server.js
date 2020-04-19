c2abd1aaead67809e9731cf1c64c63f970572733
Olga Zbanok <olga@zbanok.com>
2020-04-19 09:32:53 +0300

    feat: create connection to DB

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const { PORT } = require('./common/config');
+const { connectDB } = require('./db/db.client');
 const app = require('./app');
+require('dotenv').config();
 
 process.on('uncaughtException', err => {
   console.error(`UncaughtException captured: ${err.message}`);
@@ -9,6 +11,8 @@ process.on('unhandledRejection', reason => {
   console.error(`Unhandled rejection detected: ${reason.message}`);
 });
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+connectDB(() => {
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+});
