3b81791c4916ed66f9afad3f5279eadd5a5fc5d4
Olga Zbanok <olga@zbanok.com>
2020-04-25 20:52:40 +0300

    feat: create admin user

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const { logger } = require('../helpers/logger');
 const { MONGO_CONNECTION_STRING } = require('../common/config');
+const User = require('../resources/users/user.model');
+const createHash = require('../helpers/hash');
+
+const createAdminUser = async () => {
+  const password = 'admin';
+  const hashedPassword = await createHash(password);
+  const admin = { name: 'admin', login: 'admin', password: hashedPassword };
+
+  return await User.create(admin);
+};
 
 const connectDB = cb => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
@@ -12,10 +22,10 @@ const connectDB = cb => {
   const db = mongoose.connection;
 
   db.on('error', () => logger.info('Connection error!'));
-
-  db.once('open', () => {
+  db.once('open', async () => {
     logger.info('Сonnection successful');
-    db.dropDatabase();
+    await db.dropDatabase();
+    await createAdminUser();
     cb();
   });
 };
